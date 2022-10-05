0141c9b91e464e4a4948ae73380b132e8abf8039
DimasVhigarr <dimasvhigar2002@gmail.com>
2022-10-05 17:30:15 +0700

    methode read

--- bubbleshortA/Program.cs
+++ bubbleshortA/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace bubbleshortA
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        private int[] a = new int[20];
+
+        private int n;
+
+        public void read()
         {
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20) break;
+                else
+                {
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 element. \n");
+
+                }
+                Console.WriteLine("");
+                Console.WriteLine("----------------------");
+                Console.WriteLine("Masukkan element Array");
+                Console.WriteLine("----------------------");
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("<" + (i + 1) + ">");
+                    string s1 = Console.ReadLine();
+                    a[1] = Int32.Parse(s1);
+                }
+
+
+
+            }
         }
     }
 }
